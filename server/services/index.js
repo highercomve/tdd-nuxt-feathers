91d19a7469eb00a229ee677a416f129991b159dd
Sergio Marin <higher.vnf@gmail.com>
2017-08-25 12:26:46 -0300

    remove message service

--- server/services/index.js
+++ server/services/index.js
@@ -1,7 +1,5 @@
-const messages = require('./messages/messages.service.js');
 const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
-  app.configure(messages);
   app.configure(users);
 };
