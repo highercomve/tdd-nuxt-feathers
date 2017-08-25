6c06beedda128ebad6832237300e590cd56a6094
Sergio Marin <higher.vnf@gmail.com>
2017-08-25 18:23:22 -0300

    Testing initialization

--- test/app.test.js
+++ test/app.test.js
@@ -1,46 +1,36 @@
-const assert = require('assert');
-const rp = require('request-promise');
-const app = require('../src/app');
+import test from 'ava'
+import { Nuxt, Builder } from 'nuxt'
+import { resolve } from 'path'
 
-describe('Feathers application tests', () => {
-  before(function(done) {
-    this.server = app.listen(3030);
-    this.server.once('listening', () => done());
-  });
+// We keep the nuxt and server instance
+// So we can close them at the end of the test
+let nuxt = null
+let server = null
 
-  after(function(done) {
-    this.server.close(done);
-  });
+// Init Nuxt.js and create a server listening on localhost:4000
+test.before('Init Nuxt.js', async t => {
+  const rootDir = resolve(__dirname, '..')
+  let config = {}
+  try { config = require(resolve(rootDir, 'nuxt.config.js')) } catch (e) {}
+  config.rootDir = rootDir // project folder
+  config.dev = false // production build
+  nuxt = new Nuxt(config)
+  await new Builder(nuxt).build()
+  nuxt.listen(4000, 'localhost')
+})
 
-  it('starts and shows the index page', () => {
-    return rp('http://localhost:3030').then(body =>
-      assert.ok(body.indexOf('<html>') !== -1)
-    );
-  });
+// Example of testing only generated html
+test('Route / exits and render HTML', async t => {
+  let context = {}
+  const { html, error, redirected } = await nuxt.renderRoute('/', context)
+  t.deepEqual(redirected, {
+    path: '/login',
+    query: {},
+    status: 302,
+  })
+})
 
-  describe('404', function() {
-    it('shows a 404 HTML page', () => {
-      return rp({
-        url: 'http://localhost:3030/path/to/nowhere',
-        headers: {
-          'Accept': 'text/html'
-        }
-      }).catch(res => {
-        assert.equal(res.statusCode, 404);
-        assert.ok(res.error.indexOf('<html>') !== -1);
-      });
-    });
-
-    it('shows a 404 JSON error without stack trace', () => {
-      return rp({
-        url: 'http://localhost:3030/path/to/nowhere',
-        json: true
-      }).catch(res => {
-        assert.equal(res.statusCode, 404);
-        assert.equal(res.error.code, 404);
-        assert.equal(res.error.message, 'Page not found');
-        assert.equal(res.error.name, 'NotFound');
-      });
-    });
-  });
-});
+// Close server and ask nuxt to stop listening to file changes
+test.after('Closing server and nuxt.js', t => {
+  nuxt.close()
+})
