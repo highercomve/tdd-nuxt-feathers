cc6a26f4468debfa43d1830bb8a93f7879bc3c19
Sergio Marin <higher.vnf@gmail.com>
2017-08-26 23:31:10 -0300

    Passing all test

--- src/filter_todo_by.js
+++ src/filter_todo_by.js
@@ -1,3 +1,9 @@
-export default function filterTodoBy () {
-  
+export default function filterTodoBy (todoList, filter) {
+  if (!filter) {
+    return todoList
+  }
+  if (!!filter && typeof filter !== 'function') {
+      throw new Error('filter must be a function')
+  }
+  return todoList.filter(filter)
 }
--- test/filter_todo_by.test.js
+++ test/filter_todo_by.test.js
@@ -1,37 +1,51 @@
 import test from 'ava'
 import filterTodoBy from '../src/filter_todo_by'
 
-const list = [
-    {name: 'tarea 1', done: false}, 
-    {name: 'tarea 2', done: true},
-    {name: 'tarea 3', done: true}
+const list = [{
+    name: 'tarea 1',
+    done: false
+  },
+  {
+    name: 'tarea 2',
+    done: true
+  },
+  {
+    name: 'tarea 3',
+    done: true
+  }
 ]
 
 test('With no filter return the same', t => {
-    const filtered = filterTodoBy(list, null)
-    t.deepEqual(filtered, list)
+  const filtered = filterTodoBy(list)
+  t.deepEqual(filtered, list)
 })
 
 test('With an filter that is not a function', t => {
-    const error = t.throw(() => {
-        const filtered = filterTodoBy(list, 'no una funcion')
-    }, Error)
-    t.is(error.message, 'filter must be a function')
+  const error = t.throws(() => {
+    const filtered = filterTodoBy(list, 'no una funcion')
+  }, Error)
+  t.is(error.message, 'filter must be a function')
 })
 
 test('With filter done true', t => {
-    const filter = (todo) => todo.done === true
-    const filtered = filterTodoBy(list, filter)
-    t.deepEqual(filtered, [
-        {name: 'tarea 2', done: true},
-        {name: 'tarea 3', done: true}
-    ])
+  const filter = (todo) => todo.done === true
+  const filtered = filterTodoBy(list, filter)
+  t.deepEqual(filtered, [{
+      name: 'tarea 2',
+      done: true
+    },
+    {
+      name: 'tarea 3',
+      done: true
+    }
+  ])
 })
 
 test('With filter done false', t => {
-    const filter = (todo) => todo.done === false
-    const filtered = filterTodoBy(list, filter)
-    t.deepEqual(filtered, [
-        {name: 'tarea 1', done: false}
-    ])
+  const filter = (todo) => todo.done === false
+  const filtered = filterTodoBy(list, filter)
+  t.deepEqual(filtered, [{
+    name: 'tarea 1',
+    done: false
+  }])
 })
